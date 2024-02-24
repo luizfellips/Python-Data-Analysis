3d18ba531fd51a0fd34ade4dee5a488c1db97a8c
FELLIPS <110192027+luizfellips@users.noreply.github.com>
2024-02-24 18:28:20 -0300

    a

--- trip_analysis.py
+++ trip_analysis.py
@@ -1,26 +1,68 @@
 # default approach instead of using lib
+import math
 
 Paris = ['Paris', 200, 20, 200]
-London = ['London', 250, 30, 200]
+London = ['London', 250, 30, 120]
 Dubai = ['Dubai', 370, 15, 80]
 Mumbai = ['Mumbai', 450, 10, 70]
 
 cities = (Paris, London, Dubai, Mumbai)
 
-def calculateVacationPlan(cities, duration_in_days):
+
+# adds total cost amount to each city
+def calculateVacationCost(cities, duration_in_days):
     duration_in_weeks = duration_in_days / 7
-    total_costs = []
 
     for city in cities:
         total_amount =  city[1] + (city[2] * duration_in_days) + (city[3] * duration_in_weeks)
-        total_costs.append(total_amount)
+        city.append(total_amount)
 
-    return total_costs
+# returns cheapest city
+def getCheapestCity(cities):
+    costs = []
+    for item in cities:
+        costs.append(item[4])
 
-def getCheapestCity(cities, costs):
     return cities[costs.index(min(costs))]
 
-costs = calculateVacationPlan(cities, 7)
-cheapest_city = getCheapestCity(cities, costs)
+def maximizeOnBudget(cities, budget):
+    budgetCities = []
+    max_duration = 0
 
-print(cheapest_city)
+    for city in cities:
+        duration = math.ceil((budget / city[4]) * 7)
+
+        budgetCities.append([city[0], duration])
+        
+        if duration > max_duration:
+            max_duration = duration
+
+    max_duration_cities = [[city[0], city[1]] for city in budgetCities if city[1] == max_duration]
+
+    return max_duration_cities
+
+def minimizeOnBudget(cities, budget):
+    budgetCities = []
+    min_duration = float('inf')  # Initialize to a large value
+
+    for city in cities:
+        duration = math.ceil((budget / city[4]) * 7)
+
+        budgetCities.append([city[0], duration])
+        
+        if duration < min_duration:
+            min_duration = duration
+
+    min_duration_cities = [[city[0], city[1]] for city in budgetCities if city[1] == min_duration]
+
+    return min_duration_cities
+
+duration_in_days = 7
+budget = 400
+
+## sets total cost for cities for given duration
+calculateVacationCost(cities, duration_in_days)
+
+cheapest_city = getCheapestCity(cities)
+maximized_duration_cities = maximizeOnBudget(cities, budget)
+minimized_duration_cities = minimizeOnBudget(cities, budget)
